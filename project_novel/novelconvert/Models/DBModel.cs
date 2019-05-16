2e24e22814cf7cdf448faae89be3179cfb98a606
Britoz <lythevinh2222@gmail.com>
2019-05-16 16:34:25 +1000

    I have done the implementation for the test of User and Home, let me know if there is any error

--- project_novel/novelconvert/Models/DBModel.cs
+++ project_novel/novelconvert/Models/DBModel.cs
@@ -16,7 +16,7 @@ namespace novelconvert.Models
 
         public void DBConnection(MySqlConnection conn, string database_name)
         {
-            conn = new MySqlConnection("server=localhost;userid=root;password=;database=" + database_name);
+            conn = new MySqlConnection("server=localhost;userid=root;password=123456;database=" + database_name);
             conn.Open();
             
         }
@@ -41,13 +41,75 @@ namespace novelconvert.Models
 
         public NovelModel SelectOneNovel(string id)
         {
-            return null;
+            string query = "SELECT * FROM `novel_infor` WHERE Id='" + id + "'";
+            MySqlConnection conn = new MySqlConnection("server=localhost;userid=root;password=123456;database=novel");
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            conn.Open();
+
+            var reader = cmd.ExecuteReader();
+
+            NovelModel readNovel = new NovelModel();
+            if (reader.Read())
+            {
+                readNovel.Id = reader.GetString(0);
+                readNovel.Name = reader.GetString(1);
+                readNovel.Link = reader.GetString(2);
+                readNovel.Author = reader.GetString(3);
+                readNovel.Chap_number = Int32.Parse(reader.GetString(4));
+                readNovel.Rating = Int32.Parse(reader.GetString(5));
+                readNovel.Viewer = Int32.Parse(reader.GetString(6));
+                readNovel.Voting = Int32.Parse(reader.GetString(7));
+                readNovel.Recommandation = Int32.Parse(reader.GetString(8));
+            }
+
+
+            conn.Close();
+
+            return readNovel;
+        }
+
+        public bool AddNewNovel(NovelModel nv)
+        {
+            string connectionString = "server=localhost;userid=root;password=123456;database=novel";
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    try
+                    {
+                        string insertData = "insert into novel_infor(Name,Link,Author,Chap_number," +
+                                            "Rating, Viewer, Voting, Recommandation, image_link) values "
+                                            + "(@Name, @Link, @Author, @Chap_number, @Rating, @Viewer, @Voting,"
+                                            + "@Recommandation, @image_link)";
+                        MySqlCommand command = new MySqlCommand(insertData, connection);
+
+                        command.Parameters.AddWithValue("@Name", nv.Name);
+                        command.Parameters.AddWithValue("@Link", nv.Link);
+                        command.Parameters.AddWithValue("@Author", nv.Author);
+                        command.Parameters.AddWithValue("@Chap_number", nv.Chap_number);
+                        command.Parameters.AddWithValue("@Rating", 0);
+                        command.Parameters.AddWithValue("@Viewer", 0);
+                        command.Parameters.AddWithValue("@Voting", 0);
+                        command.Parameters.AddWithValue("@Recommandation", 0);
+                        command.Parameters.AddWithValue("@image_link", nv.Image_link);
+
+                        connection.Open();
+                        int result = command.ExecuteNonQuery();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        return false;
+                    }
+            }
+
+            return true;
         }
 
         public List<NovelModel> AllNovel()
         {
             string query = "SELECT * FROM `novel_infor` WHERE 1";
-            MySqlConnection conn = new MySqlConnection("server=localhost;userid=root;password=;database=novel");
+            MySqlConnection conn = new MySqlConnection("server=localhost;userid=root;password=123456;database=novel");
 
             MySqlCommand cmd = new MySqlCommand(query, conn);
             conn.Open();
@@ -77,6 +139,68 @@ namespace novelconvert.Models
             DBCloseConnection(conn);
          
             return lresult;
+        }
+
+        public bool EditNovel(NovelModel nv, NovelModel newNv)
+        {
+            string connectionString = "server=localhost;userid=root;password=123456;database=novel";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    string insertData = "UPDATE novel_infor SET `Name`=@Name,"
+                        + "`Link`=@Link,`Author`=@Author,"
+                        + "`image_link`=@image_link WHERE id=@idold";
+                    MySqlCommand command = new MySqlCommand(insertData, connection);
+                    //select old novel
+                    command.Parameters.AddWithValue("@idold", nv.Id);
+                    //data updated
+                    string name = (newNv.Name != null) ? newNv.Name : nv.Name; //checking null
+                    command.Parameters.AddWithValue("@Name", name);
+
+                    string Link = (newNv.Link != null) ? newNv.Link : nv.Link;
+                    command.Parameters.AddWithValue("@Link", Link);
+
+                    string Author = (newNv.Author != null) ? newNv.Author : nv.Author;
+                    command.Parameters.AddWithValue("@Author", Author);
+
+                    string Image_link = (newNv.Image_link != null) ? newNv.Image_link : nv.Image_link;
+                    command.Parameters.AddWithValue("@image_link", newNv.Image_link);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool RemoveNovelById(string id)
+        {
+            string connectionString = "server=localhost;userid=root;password=123456;database=novel";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    string insertData = "DELETE FROM novel_infor WHERE id=@id";
+                    MySqlCommand command = new MySqlCommand(insertData, connection);
+                    command.Parameters.AddWithValue("@id", id);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
---
+++ project_novel/novelconvert/Models/UserDBModel.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace novelconvert.Models
+{
+    public class UserDBModel
+    {
+        //register user by username and password
+        public UserModel UserRegister(string username, string password)
+        {
+            string connectionString = "server=localhost;userid=root;password=123456;database=novel";
+            if (!UserExisting(username, password))
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    try
+                    {
+                        string insertData = "insert into user_infor(UserName,Password,Date_of_birth,Image_profile," +
+                                            "Coin, Level, Experience, Type, Power, Nick_name) values "
+                                            + "(@UserName, @Password, @Date_of_birth, @Image_profile, @Coin, @Level, @Experience, @Type, @Power, @Nick_name)";
+                        MySqlCommand command = new MySqlCommand(insertData, connection);
+
+                        command.Parameters.AddWithValue("@UserName", username);
+                        command.Parameters.AddWithValue("@Password", password);
+                        command.Parameters.AddWithValue("@Date_of_birth", "1993-11-11");
+                        command.Parameters.AddWithValue("@Image_profile", "#");
+                        command.Parameters.AddWithValue("@Coin", 100);
+                        command.Parameters.AddWithValue("@Level", 1);
+                        command.Parameters.AddWithValue("@Experience", 100);
+                        command.Parameters.AddWithValue("@Type", "normal");
+                        command.Parameters.AddWithValue("@Power", 100);
+                        command.Parameters.AddWithValue("@Nick_name", "Lee");
+
+                        connection.Open();
+                        int result = command.ExecuteNonQuery();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+
+                }
+            }
+
+            return UserLogin(username, password);
+        }
+        //checking by username and password
+        public bool UserExisting(string username, string password)
+        {
+            return UserLogin(username, password).fUsername == username; //true for exist user
+        }
+        //checking by only username
+        public bool UserNameExistChecking(string username)
+        {
+            return UserLogin(username) != null; //true for exist username
+        }
+        //get user by username and password
+        public UserModel UserLogin(string username, string password)
+        {
+            string query = "SELECT * FROM `user_infor` WHERE UserName ='" + username + "' and Password ='" + password + "'";
+            MySqlConnection conn = new MySqlConnection("server=localhost;userid=root;password=123456;database=novel");
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            conn.Open();
+
+            var reader = cmd.ExecuteReader();
+
+            UserModel userNovel = new UserModel();
+            if (reader.Read())
+            {
+
+                userNovel.fID = reader.GetString(0);
+                userNovel.fUsername = reader.GetString(1);
+                userNovel.fPassword = reader.GetString(2);
+                userNovel.fDate_of_birth = reader.GetString(3);
+                userNovel.fImage_profile = reader.GetString(4);
+
+                userNovel.fCoin = Int32.Parse(reader.GetString(5));
+                userNovel.fLevel = Int32.Parse(reader.GetString(6));
+                userNovel.fExperience = Int32.Parse(reader.GetString(7));
+                userNovel.fType = reader.GetString(8);
+                userNovel.fPower = Int32.Parse(reader.GetString(9));
+                userNovel.fNick_name = reader.GetString(4);
+            }
+
+            conn.Close();
+            return userNovel;
+        }
+        //get user by username
+        public UserModel UserLogin(string username)
+        {
+            string query = "SELECT * FROM `user_infor` WHERE UserName ='" + username + "'";
+            MySqlConnection conn = new MySqlConnection("server=localhost;userid=root;password=123456;database=novel");
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            conn.Open();
+
+            var reader = cmd.ExecuteReader();
+
+            UserModel userNovel = new UserModel();
+            if (reader.Read())
+            {
+                userNovel.fID = reader.GetString(0);
+                userNovel.fUsername = reader.GetString(1);
+                userNovel.fPassword = reader.GetString(2);
+                userNovel.fDate_of_birth = reader.GetString(3);
+                userNovel.fImage_profile = reader.GetString(4);
+
+                userNovel.fCoin = Int32.Parse(reader.GetString(5));
+                userNovel.fLevel = Int32.Parse(reader.GetString(6));
+                userNovel.fExperience = Int32.Parse(reader.GetString(7));
+                userNovel.fType = reader.GetString(8);
+                userNovel.fPower = Int32.Parse(reader.GetString(9));
+                userNovel.fNick_name = reader.GetString(4);
+            }
+
+            conn.Close();
+            return userNovel;
+        }
+    }
+}
